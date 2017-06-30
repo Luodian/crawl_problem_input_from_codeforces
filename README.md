8f3d2032cf19c88a519c818a71263b9b35c2a064
PracNeyman <1799022371@qq.com>
2017-06-30 10:02:44 +0800

    finish output part

---
+++ README.md
@@ -0,0 +1,6 @@
+# Crawl the sample input and output from codeforces.
+
+![pic](http://opmza2br0.bkt.clouddn.com/17-6-30/58450175.jpg)
+
+![pic](http://opmza2br0.bkt.clouddn.com/17-6-30/6353404.jpg)
+
--- input.py
+++ input.py
@@ -8,21 +8,25 @@ headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'}
 
 
-def crawler(contest_num, problem_num):
+def crawler(contest_num,problem_num):
     url = "http://codeforces.com/contest/" + str(contest_num) + "/problem/" + problem_num
     print(url + " Veridict.")
     content = requests.get(url, headers=headers).text
     tree = etree.HTML(content)
     sample_input = tree.xpath('//div[@class = "input"]//pre/text()')
-    f = open('/Users/luodian/desktop/in.txt','w')
+    sample_output = tree.xpath('//div[@class = "output"]//pre/text()')
+    f = open('/Users/luodian/desktop/' + str(problem_num) + '.txt','w')
     print ("Sample Input: ")
     for item in sample_input:
         f.writelines(item + "\n")
         print (item)
 
+    print ("Sample Output: ")
+    for item in sample_output:
+        print (item)
 
 
 if __name__ == "__main__":
-    # c = input("Please input the contest number: ")
-    # p = input("Please input the problem number: ")
-    crawler(819, 'A')
+    c = input("Please input the contest number: ")
+    for num,item in enumerate("ABCDE"):
+        crawler(c,item)
